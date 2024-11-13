10987231449ed750ada8df52a8041f7e4559b283
Usama Ahmed Siddiquie <220usamaahmed@gmail.com>
2024-11-13 01:30:38 +0100

    Get env running with laser line renders

---
+++ main.py
@@ -0,0 +1,92 @@
+from f110_gym.envs.base_classes import Integrator
+import yaml
+import gym
+import numpy as np
+from argparse import Namespace
+from pyglet.gl import GL_LINES
+
+
+NUM_AGENTS = 1
+NUM_BEAMS = 1080
+LASER_FOV = 4.7
+
+beam_gl_lines = []
+
+
+def load_map_config(map_name: str) -> Namespace:
+    with open("map_configs.yaml") as file:
+        maps_config = yaml.load(file, Loader=yaml.FullLoader)
+
+    map_config = maps_config.get(map_name)
+    assert map_config is not None, f"Could not load config for map {map_name}"
+
+    return Namespace(**map_config)
+
+
+def render_callback(env_renderer):
+    global beam_gl_lines
+
+    if not len(beam_gl_lines):
+        for _ in range(NUM_BEAMS):
+            gl_line = env_renderer.batch.add(
+                2,
+                GL_LINES,
+                None,
+                ("v2f/stream", (0, 0, 0, 0)),
+                ("c3B/stream", (255, 0, 0, 255, 0, 0)),
+            )
+            beam_gl_lines.append(gl_line)
+
+
+def update_beam_gl_lines(obs):
+    global beam_gl_lines
+    assert len(beam_gl_lines) == NUM_BEAMS
+
+    car_x = obs["poses_x"][0]
+    car_y = obs["poses_y"][0]
+    car_theta = obs["poses_theta"][0]
+    scans = obs["scans"][0]
+
+    assert len(scans) == NUM_BEAMS
+
+    for beam_i in range(0, NUM_BEAMS, 10):
+        gl_line = beam_gl_lines[beam_i]
+        theta = car_theta + ((beam_i / NUM_BEAMS) * LASER_FOV) - (LASER_FOV / 2)
+
+        end_x = car_x + np.cos(theta) * scans[beam_i]
+        end_y = car_y + np.sin(theta) * scans[beam_i]
+
+        gl_line.vertices = [car_x * 50, car_y * 50, end_x * 50, end_y * 50]
+
+
+def run_environment(config: Namespace):
+    env = gym.make(
+        "f110_gym:f110-v0",
+        map=config.map_path,
+        map_ext=config.map_ext,
+        num_agents=1,
+        timestep=0.01,
+        integrator=Integrator.RK4,
+    )
+    env.add_render_callback(render_callback)
+
+    obs, step_reward, done, info = env.reset(
+        np.array([[config.starting_x, config.starting_y, config.starting_theta]])
+    )
+    env.render()
+
+    while not done:
+        speed, steer = 0.5, 0.1
+        obs, step_reward, done, info = env.step(np.array([[steer, speed]]))
+
+        update_beam_gl_lines(obs)
+        env.render(mode="human")
+
+
+def main():
+    config = load_map_config("circle")
+    run_environment(config)
+
+
+if __name__ == "__main__":
+    main()
---
+++ notes/SETUP.md
@@ -0,0 +1,56 @@
+# Humanoid Robotics Lab (F1Tenth)
+
+## Dependencies
+openai gym > f1tenth_gym > f1tenth_gym_ros > ws24_racing
+
+### f1tenth_gym
+
+#### Running this repo
+```bash
+conda create --name f1tenth python=3.8
+conda activate f1tenth
+
+# Change 0.19.0 -> 0.26.2 in setup.py
+
+cd f1tenth_gym
+pip install -e .
+
+pip install pyglet==1.5.20
+
+pip install pip==19.2.3
+pip install setuptools==65.5.0
+
+pip install gym==0.19.0 # Ignore any errors related to opencv
+
+cd examples
+python3 waypoint_follow.py
+```
+
+#### Custom maps
+[Build custom maps](https://f1tenth-gym.readthedocs.io/en/latest/customized_usage.html)
+
+#### Final Set of Dependencies
+```
+------------------ ------- ------------------------------------------------------------
+cloudpickle        1.6.0
+f110-gym           0.2.1   /Users/usama/UniversityWork/HumanoidRobotics/f1tenth_gym/gym
+future             1.0.0
+gym                0.19.0
+gym-notices        0.0.8
+importlib-metadata 8.5.0
+llvmlite           0.41.1
+numba              0.58.1
+numpy              1.22.0
+pillow             10.4.0
+pip                19.2.3
+pyglet             1.5.20
+PyOpenGL           3.1.7
+PyYAML             6.0.2
+scipy              1.10.1
+setuptools         65.5.0
+wheel              0.44.0
+zipp               3.20.2
+```
+
+Install f1tenth_gym with gym 0.26.2, then downgrade pip and setuptools and install gym 0.19.
+Trying to directly install 0.19 leads to setup.py failing and nothing getting installed.
